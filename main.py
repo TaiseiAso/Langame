06f231fdc93db2de21c636843922051bba2a6165
TaiseiAso <taisei.aso.study@gmail.com>
2019-11-08 15:32:03 +0900

    #1 平仮名入力システムの実装

---
+++ main.py
@@ -0,0 +1,140 @@
+# coding: utf-8
+
+
+# https://support.microsoft.com/ja-jp/help/883232
+
+
+import pygame
+from pygame.locals import *
+import sys
+
+
+class Key():
+    def __init__(self):
+        self.press = []
+        self.update()
+
+    def update(self):
+        self.push = []
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                self.press.append(event.key)
+                self.push.append(event.key)
+            elif event.type == KEYUP:
+                self.press.remove(event.key)
+            elif event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+class Text():
+    def __init__(self):
+        self.font = pygame.font.SysFont("hg正楷書体pro", 50)
+
+    def draw(self, screen, str, dest):
+        text = self.font.render(str, True, (255,255,255))
+        screen.blit(text, dest)
+
+class User():
+    def __init__(self, key):
+        self.init_message()
+        self.key = key
+        self.text = Text()
+        self.load_romaji()
+
+    def load_romaji(self):
+        self.romaji = []
+        with open("romaji.txt", 'r', encoding='utf-8') as f:
+            line = f.readline()
+            while line:
+                line = line.strip()
+                alphabets, hiragana = line.split(':')
+                for alphabet in alphabets.split('^'):
+                    self.romaji.append((alphabet, hiragana))
+                line = f.readline()
+
+    def init_message(self):
+        self.ja_mes = ""
+        self.en_mes = ""
+
+    def check_message(self):
+        return self.ja_mes != "" and self.en_mes == ""
+
+    def print_message(self):
+        print(self.ja_mes + self.en_mes)
+
+    def backspace(self):
+        if self.en_mes == "":
+            self.ja_mes = self.ja_mes[:-1]
+        else:
+            self.en_mes = self.en_mes[:-1]
+
+    def add_character(self, ch):
+        if len(self.ja_mes) >= 12:
+            return
+
+        if self.en_mes == "n" and ch not in ['a','i','u','e','o','y','n']:
+            self.ja_mes += "ん"
+            self.en_mes = ch
+            return
+
+        next_mes = self.en_mes + ch
+        for alphabet, hiragana in self.romaji:
+            if next_mes == alphabet[:len(next_mes)]:
+                if len(next_mes) == len(alphabet):
+                    self.ja_mes += hiragana
+                    self.en_mes = ""
+                else:
+                    self.en_mes = next_mes
+                break
+
+    def type_message(self):
+        message = None
+        for key_id in self.key.push:
+            if key_id == K_BACKSPACE:
+                self.backspace()
+            elif key_id in list(range(K_a, K_z+1)) + [K_1, K_SLASH, K_COMMA, K_MINUS]:
+                self.add_character(chr(key_id))
+            elif key_id == K_RETURN:
+                if self.check_message():
+                    message = self.ja_mes
+                    self.init_message()
+        return message
+
+    def draw(self, screen):
+        self.text.draw(screen, self.ja_mes + self.en_mes, [20,20])
+
+
+class Game():
+    def __init__(self, name, size, fps):
+        pygame.init()
+        pygame.display.set_mode(size, 0, 32)
+        pygame.display.set_caption(name)
+        self.screen = pygame.display.get_surface()
+        self.key = Key()
+        self.user = User(self.key)
+        self.fps = fps
+
+    def input(self):
+        self.key.update()
+        message = self.user.type_message()
+        if message:
+            print(message)
+
+    def draw(self):
+        self.screen.fill((0,0,0,0))
+        self.user.draw(self.screen)
+        pygame.display.update()
+
+    def wait(self):
+        pygame.time.wait(int(1000 / self.fps))
+
+    def play(self):
+        while True:
+            self.input()
+            self.draw()
+            self.wait()
+
+
+if __name__ == "__main__":
+    game = Game("LANGAME DUNGEON", (800,400), 30)
+    game.play()
